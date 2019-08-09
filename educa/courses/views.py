0f4635a6b51c86d6770a7762db5c61b11b1c522d
Sadman Chowdhury <sadman923@gmail.com>
2019-08-09 21:17:10 +0600

    Added form to edit course modules

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -7,6 +7,37 @@ from django.contrib.auth.mixins import LoginRequiredMixin, \
 from django.views.generic.edit import CreateView, UpdateView, \
                                             DeleteView
 
+## Inline Form
+from django.shortcuts import redirect, get_object_or_404
+from django.views.generic.base import TemplateResponseMixin, View
+from .forms import ModuleFormSet
+
+class CourseModuleUpdateView(TemplateResponseMixin, View):
+    template_name = 'courses/manage/module/formset.html'
+    course = None
+
+    def get_formset(self, data=None):
+        return ModuleFormSet(instance=self.course, data=data)
+
+    def dispatch(self, request, pk):
+        self.course = get_object_or_404(Course, id=pk, owner=request.user)
+        return super(CourseModuleUpdateView, self).dispatch(request, pk)
+
+    def get(self, request, *args, **kwargs):
+        formset = self.get_formset()
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+    def post(self, request, *args, **kwargs):
+        formset = self.get_formset(data=request.POST)
+        if formset.is_valid():
+            formset.save()
+            return redirect('manage_course_list')
+        return self.render_to_response({'course': self.course,
+                                        'formset': formset})
+
+
+
 # Owner mixins allows to define the behaviour of class
 class OwnerMixin(object):
     """
