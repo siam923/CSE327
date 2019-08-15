69cbb39e47427bcf3f34d25cbe1b487775f44c39
Sadman Chowdhury <sadman923@gmail.com>
2019-08-15 21:38:33 +0600

    added display view of contents

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -26,4 +26,7 @@ urlpatterns = [
     path('content/<int:id>/delete/',
           views.ContentDeleteView.as_view(),
           name='module_content_delete'),
+    path('module/<int:module_id>/',
+          views.ModuleContentListView.as_view(),
+          name='module_content_list'),
 ]
