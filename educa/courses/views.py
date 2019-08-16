56a0beff67df5c3a40b369f76424b729de1bd96b
Sadman Chowdhury <sadman923@gmail.com>
2019-08-16 21:03:58 +0600

    updated public view of courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -19,6 +19,12 @@ from .models import Module, Content
 
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
+# Course display view
+from django.views.generic.detail import DetailView
+from django.db.models import Count
+from .models import Subject
+
+
 
 
 class CourseModuleUpdateView(TemplateResponseMixin, View):
@@ -196,3 +202,26 @@ class ContentOrderView(CsrfExemptMixin,
                        module__course__owner=request.user) \
                        .update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+## course display
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(
+                            total_courses=Count('courses'))
+        courses = Course.objects.annotate(
+                            total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
